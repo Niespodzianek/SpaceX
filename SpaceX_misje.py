8976a0560b43bc0004436dfdb8c80dced9332c91
Niespodzianek <michal_grzyb@yahoo.pl>
2022-02-19 02:19:22 +0100

    Drobne zmiany.

--- SpaceX_misje.py
+++ SpaceX_misje.py
@@ -1,16 +1,21 @@
 import requests
 import json
 
+
 def odczytanie_infornacji_o_misjach(adres_strony):
     misje = json.loads(requests.get(adres_strony).text)
     return misje
 
+
 def prezentacja_misji(misje):
     for index in range(len(misje)):
-        print(f"Pozycja {index + 1}. Nazwa misji: {misje[index]['mission_name']},"
-              f" Symbol: {misje[index]['mission_id']},"
-              f" Inwestor: {misje[index]['manufacturers']},"
-              f" Ładunek: {misje[index]['payload_ids']}")
+        print(
+            f"Pozycja {index + 1}. Nazwa misji: {misje[index]['mission_name']},"
+            f" Symbol: {misje[index]['mission_id']},"
+            f" Inwestor: {misje[index]['manufacturers']},"
+            f" Ładunek: {misje[index]['payload_ids']}"
+        )
+
 
 def program():
     api = "https://api.spacexdata.com/v3/missions"
@@ -18,5 +23,6 @@ def program():
     prezentacja_misji(zaplanowane_misje)
     print("KONIEC PRACY")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     program()
--- main.py
+++ main.py
@@ -1,26 +1,31 @@
 import requests
 import json
 
-def odczytanie_infornacji(adres_strony):
+
+def odczytanie_informacji(adres_strony):
     strona = requests.get(adres_strony)
     zawartosc = strona.text
     misje = json.loads(zawartosc)
 
     for index in range(len(misje)):
-        print(f"Pozycja {index + 1}. Nazwa misji: {misje[index]['mission_name']},"
-              f" Symbol: {misje[index]['mission_id']},"
-              f" Inwestor: {misje[index]['manufacturers']},"
-              f" Ładunek: {misje[index]['payload_ids']}")
+        print(
+            f"Pozycja {index + 1}. Nazwa misji: {misje[index]['mission_name']},"
+            f" Symbol: {misje[index]['mission_id']},"
+            f" Inwestor: {misje[index]['manufacturers']},"
+            f" Ładunek: {misje[index]['payload_ids']}"
+        )
 
         # print(f"Opis: {misje[index]['description']}")
         # for klucz, wartosc in misje[index].items():
         #     print(f"Klucz - {klucz}, wartość - {wartosc}")
         # input("\nNaciśnij ENTER aby kontynuować")
 
+
 def program():
     api = "https://api.spacexdata.com/v3/missions"
-    odczytanie_infornacji(adres_strony=api)
+    odczytanie_informacji(adres_strony=api)
     print("KONIEC PRACY")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     program()
